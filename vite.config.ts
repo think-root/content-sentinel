2184c346c0dacb2ba76e2577de2333c9f4882eee
sigmanor <sigmanor@gmail.com>
2025-03-24 17:30:36 +0200

    refactor(vite.config): update configuration for dashboard build and asset management
    Modified the Vite configuration to set a base path for the dashboard, specify output directories for build assets, and maintain existing server proxy settings. This enhances the structure for serving static files and improves overall project organization.

--- vite.config.ts
+++ vite.config.ts
@@ -6,25 +6,30 @@ export default defineConfig(({ mode }) => {
   const apiBaseUrl = env.API_BASE_URL || 'http://localhost:9111/think-root';
 
   return {
-  plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom'],
-  },
-  define: {
-    'import.meta.env.API_BEARER_TOKEN': JSON.stringify(env.API_BEARER_TOKEN),
-    'import.meta.env.API_BASE_URL': JSON.stringify(env.API_BASE_URL),
-    'import.meta.env.DATE_FORMAT': JSON.stringify(env.DATE_FORMAT),
-    'import.meta.env.TIMEZONE': JSON.stringify(env.TIMEZONE),
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: `${apiBaseUrl}/api`,
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+    plugins: [react()],
+    base: "/dashboard/",
+    build: {
+      assetsDir: "assets",
+      outDir: "dist",
+    },
+    resolve: {
+      dedupe: ["react", "react-dom"],
+    },
+    define: {
+      "import.meta.env.API_BEARER_TOKEN": JSON.stringify(env.API_BEARER_TOKEN),
+      "import.meta.env.API_BASE_URL": JSON.stringify(env.API_BASE_URL),
+      "import.meta.env.DATE_FORMAT": JSON.stringify(env.DATE_FORMAT),
+      "import.meta.env.TIMEZONE": JSON.stringify(env.TIMEZONE),
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: `${apiBaseUrl}/api`,
+          changeOrigin: true,
+          secure: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
   };
 });
